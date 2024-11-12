1e32f2e1add2a6985b0ceec4ba3adfebb4ca1b7d
Taew00k <davis0625@dgu.ac.kr>
2024-11-12 20:58:26 +0900

    feat: 회원가입(이름) 구현 완료

--- week4/assignment/week4_assignment/src/pages/SignUp/NamePage.tsx
+++ week4/assignment/week4_assignment/src/pages/SignUp/NamePage.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
+import { Theme } from '../../styles/Themes';
 
 interface FormData {
   id: string;
@@ -19,26 +20,93 @@ const NamePage = ({ onChange, onNext, value }: NamePageProps) => {
   const MovetoLogin = () => {
     navigate('/');
   };
-  
+
   return (
     <div>
-      <h2>이름</h2>
-      <div>
-      <input
-        type="text"
-        placeholder="사용자 이름을 입력해주세요"
-        value={value}
-        onChange={(e) => onChange('id', e.target.value)}
-      />
-      {value.length > 8 && <span>이름은 8자 이하로 입력해주세요</span>}
-    </div>
-    <div><button>다음</button></div>
-    <div>
-    <span>이미 회원이신가요?</span>
-    <span onClick={MovetoLogin}>로그인</span>
-    </div>
+      <H2>이름</H2>
+      <InputContainer>
+        <Input
+          type="text"
+          placeholder="사용자 이름을 입력해주세요"
+          value={value}
+          onChange={(e) => onChange('id', e.target.value)}
+        />
+        {value.length > 8 && <span>이름은 8자 이하로 입력해주세요</span>}
+      </InputContainer>
+      <ButtonContainer>
+        <Button disabled={value.length < 0 || value.length > 8} onClick={onNext}>
+          다음
+        </Button>
+      </ButtonContainer>
+      <LoginContainer>
+        <span>이미 회원이신가요?</span>
+        <span onClick={MovetoLogin}>로그인</span>
+      </LoginContainer>
     </div>
   );
 };
 
 export default NamePage;
+
+const H2 = styled.h2`
+  ${Theme.font.medium};
+`;
+
+const Input = styled.input`
+  width: 20rem;
+  height: 3rem;
+  margin-top: 0.5rem;
+  border: 1px solid ${Theme.color.Lightgray};
+  border-radius: 0.5rem;
+  padding-left: 1rem;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 1rem;
+`;
+
+const Button = styled.button`
+  width: 20rem;
+  height: 3rem;
+  border: 1px solid ${Theme.color.Lightgray};
+  border-radius: 0.5rem;
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+  background-color: ${({ disabled }) => (disabled ? Theme.color.Gray : Theme.color.Yellowgreen)};
+  color: ${Theme.color.White};
+  transition: background-color 0.4s ease;
+
+  ${({ disabled }) =>
+    !disabled &&
+    `
+    &:hover {
+      background-color: ${Theme.color.Darkbrown};
+    }
+  `}
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  span {
+    ${Theme.font.smallred};
+    margin-top: 0.2rem;
+    margin-left: 0.2rem;
+  }
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 0.8rem;
+
+  span {
+    ${Theme.font.small};
+    margin-right: 0.5rem;
+  }
+
+  span:nth-of-type(2) {
+    color: ${Theme.color.Darkbrown};
+    cursor: pointer;
+  }
+`;
--- week4/assignment/week4_assignment/src/styles/Themes.tsx
+++ week4/assignment/week4_assignment/src/styles/Themes.tsx
@@ -7,7 +7,8 @@ const color = {
     Red: '#FF0000',
     Lightgray: '#E6E6E6',
     Darkbrown: '#61380B',
-    Lightblack: '#424242'
+    Lightblack: '#424242',
+    Gray: '#BDBDBD'
 };
 
 const font = {
@@ -26,6 +27,18 @@ const font = {
     font-weight: 500;
     line-height: 100%;
   `,
+  smallred: css`
+  font-size: 0.7rem;
+  font-weight: 500;
+  line-height: 160%;
+  color: red;
+  `,
+  small: css`
+  font-size: 0.8rem;
+  font-weight: 500;
+  line-height: 100%;
+  color: black;
+  `
 };
 
 export const Theme = {
